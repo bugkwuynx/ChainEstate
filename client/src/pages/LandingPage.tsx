e3e30bfea9e9dcaa1f98ce966a3e7a1f7680072f
bugkwuynx <hngo_2028@depauw.edu>
2026-01-13 21:36:36 -0600

    build and style navbar

---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavBar } from "../components";
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default LandingPage;
